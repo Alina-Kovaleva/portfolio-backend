c0ac586f9119c7005c7b2a30ff15eb1646eced95
Alina Kovaleva <calipco.xp@gmail.com>
2022-08-20 14:42:30 +0200

    Server part for portfolio project
    	- Added schemas for sanity

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Frontend repo](https://github.com/Alina-Kovaleva/portfolio-frontend)
+
+The front-end side of this project is using React.
---
+++ schemas/experiences.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'experiences',
+  title: 'Experiences',
+  type: 'document',
+  fields: [
+    {
+      name: 'year',
+      title: 'Year',
+      type: 'string',
+    },
+    {
+      name: 'works',
+      title: 'Works',
+      type: 'array',
+      of: [{ type: 'workExperience' }],
+    },
+  ],
+  orderings: [
+    {
+      title: 'Year',
+      name: 'year',
+      by: [{ field: 'year', direction: 'desc' }],
+    },
+    {
+      title: 'LOLOL',
+      name: 'lololo',
+      by: [{ field: 'year', direction: 'asc' }],
+    },
+  ],
+};
